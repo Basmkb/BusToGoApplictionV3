08e54344a9ad0cfe8e0a4c59bbd18bbc04d303f7
Basmkb <basgenz11@gmail.com>
2017-09-07 22:26:23 +0700

    Up1

--- app/src/main/java/com/example/user/bustogoapplictionv3/Detail.java
+++ app/src/main/java/com/example/user/bustogoapplictionv3/Detail.java
@@ -2,6 +2,10 @@ package com.example.user.bustogoapplictionv3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class Detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
+        TextView titleTextView = (TextView) findViewById(R.id.txtTitleDetail);
+        TextView detailTextView = (TextView)findViewById(R.id.txtDetailSco);
+        ImageView imageView = (ImageView)findViewById(R.id.imvimageDetail);
+        Button button =(Button)findViewById(R.id.btnBack);
+
+       titleTextView.setText(getIntent().getStringExtra("Title"));
+       detailTextView.setText(getIntent().getStringExtra("Detail"));
+
     }
+
+    public void clickBack (View view) {finish();}
+
 }
